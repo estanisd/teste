1a2fb9e5faded615e1c248f6fa380396409e15a8
Matheus <m268851@dac.unicamp.br>
2025-11-07 14:24:32 -0300

    Alterações Matheus

--- f_euleri.py
+++ f_euleri.py
@@ -1,5 +1,9 @@
 def qualquercoisa():
-    print(35)
+    print(2)
+    print('Oi')
     for i in range(35):
         print(i)
     return 45
+
+def qualquer_altracao():
+    print('Oi, meu nome é Matheus')
